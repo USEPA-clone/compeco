08db44b8dc3c99c41817ae448f9ecd4ee3a723ec
Jeff Hollister <jeff.w.hollister@gmail.com>
2021-11-30 15:00:38 -0500

    file rename, started std_curve function

--- R/ce_convert_rfus.R
+++ R/ce_convert_rfus.R
@@ -14,11 +14,12 @@
 #'                     ACESD.  The "ours" one was purchased by the compeco lab, 
 #'                     the "theirs", has been in use for sometime and currently 
 #'                     resides in M07. The default is "ours".
-#' @param date Date of the standard curve.  Acceptable values are:  "NEED THESE"
+#' @param year Year of the standard curve.  If more than one curve caluclated in
+#'             a year add "a", "b", ...  Acceptable values are:  "2021"
 #' @param output An options output path and csv file name for converted values.
 #' @return returns a tibble with proper metadata, input RFUs and converted
 #'          concentrations                     
-#' @note While it is possible to mix and match modules and fluormeters, don't 
+#' @note While it is possible to mix and match modules and fluorometers, don't 
 #'        do this.  Keep the "ours" modules with the "ours" fluorometer 
 #'        and vice-versa. 
 #' @importFrom readr read_csv write_csv
@@ -28,12 +29,14 @@
 #' ce_convert_rfus(rfu_in = examp_data)
 ce_convert_rfus <- function(rfu_in, 
                             module = c("ext_chla", "invivo_chla", "phyco"),
+                            year = c("2021"),
                             fluorometer = c("ours", "theirs"),
-                            date = c("2021-11-16"),
                             output = NULL){
   module <- match.arg(module)
+  year <- match.arg(year)
   fluorometer <- match.arg(fluorometer)
-  date <- match.arg(date)
+  std_curve <- ce_create_std_curve(module, year, fluorometer)
+  
   rfus <- suppressMessages(read_csv(rfu_in))
   browser()
   
@@ -41,4 +44,17 @@ ce_convert_rfus <- function(rfu_in,
   
   if(!is.null(output)) write_csv(out, output)
   conc
+}
+
+#' Create a standard curve
+#'
+#' This function creates a standard curve from input fluorometer and 
+#' spectrophotometer .csv files.  Curve is used to convert RFUs to µg/L.
+#' 
+#' @param ... Arguments for module, fluorometer and date of input standard curve
+#'            csv files.  Passed from \code{\link{ce_convert_rfus}}
+#' @keywords internal
+ce_create_std_curve <- function(...){
+  browser()
+  
 }
