52313980b7c194956f2a13fd7e47e8fe9c5bf785
Jeff Hollister <jeff.w.hollister@gmail.com>
2022-04-20 14:31:03 -0400

    commiting code with metadata file update even though I am killing it.

--- R/ce_read_exo_data.R
+++ R/ce_read_exo_data.R
@@ -6,29 +6,42 @@
 #' @param exo_file Path and file name for an exo file to read
 #' @param source How were the data retrieved from the sensor.  Current options 
 #'               include "handheld" or "korexo"
-#' @param sensor_meta Logical to save sensor metadata included with input EXO 
-#'                    file. Defaults to TRUE.
-#' @param sensor_meta_file Path where sensor metadat is stored.  Defaults to a 
-#'                         metadata folder in the same folder as the 
-#'                         \code{exo_file}.                    
 #' @export
 #' @examples 
 #' exo_file_path <- system.file("extdata/210915-112052-_.csv", package = "compeco")
 #' ce_read_exo_data(exo_file_path)
-ce_read_exo_data <- function(exo_file, source = c("handheld", "korexo"),
-                             sensor_meta = TRUE, 
-                             sensor_meta_file = paste0(dirname(exo_file),
-                                                       "/metadata/meta_",
-                                                       basename(exo_file))){
+ce_read_exo_data <- function(exo_file, source = c("handheld", "korexo")){
+  browser()
   source <- match.arg(source)
   # Deal with Metadata file
   if(sensor_meta & !dir.exists(dirname(sensor_meta_file))){
     dir.create(dirname(sensor_meta_file))
-  } else if(T){
-    browser()
-    #TODO Check for file existence, if exists message but make sure to change
-    #TODO names and add (e.g. _01, _02 etc.)
+  } else if(sensor_meta){
+    if(file.exists(sensor_meta_file)){
+      message(paste0("The ", basename(sensor_meta_file), " file exists.  Creating new file with incremented ID added."))
+      existing_files <- list.files(dirname(sensor_meta_file), 
+                                   pattern = gsub(".csv", "", 
+                                                  basename(sensor_meta_file)))
+      existing_files <- gsub(".csv", "", existing_files)
+      if(length(existing_files) > 1){
+        number <- stringr::str_extract(existing_files, "[0-9]*$")
+        number <- as.numeric(number)
+        number <- as.character(number + 1)
+        if(nchar(number) == 1){number <- paste0("0", number)}
+        sensor_meta_file <- paste0(tools::file_path_sans_ext(sensor_meta_file), 
+                                   number, ".",
+                                   tools::file_ext(sensor_meta_file))
+      } else {
+        sensor_meta_file <- paste0(tools::file_path_sans_ext(sensor_meta_file), 
+                                   "01.", tools::file_ext(sensor_meta_file))
+      }
+    }
+    meta <- readLines(exo_file, skipNul = TRUE)
+    idx <- which(stringr::str_detect(meta, "^Date")) - 1
+    writeLines(meta[1:idx], sensor_meta_file,sep = ",")
+    
   }
+  
   
   # Pull out metadata portion and save
   
